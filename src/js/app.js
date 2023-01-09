c096104552204161ea26c405b9e7ed398378ba8a
Olha Balumatkinnnna <balumatkinaolga@gmail.com>
2023-01-09 22:56:49 +0200

    added working burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 import '../styles/header.scss';
-import '../styles/mobile-menu.scss';
 import '../styles/main-section.scss';
 import '../styles/about-us.scss';
 import '../styles/safety.scss';
@@ -7,25 +6,14 @@ import '../styles/spins.scss';
 import '../styles/footer.scss';
 import '../styles/reset.scss';
 
-// const sum = (a, b) => a + b;
 
-// window.addEventListener('load', () => {
-//     console.log(`Sum function 1+3 = ${sum(1,3)}`);
-// })
+// mobile menu
+let link = document.getElementById('hamburger-link')
+let burger = document.getElementById('burger')
+let ul = document.getElementById('burger-list')
 
-// for mobile menu
-var burgerBtn = document.getElementById('burgerBtn');
-var mobile = document.getElementById('mobile');
-var demo2 = document.getElementById('demo2');
-
-burgerBtn.addEventListener('click', function() {
-  mobile.classList.toggle('navigation');
-}, false);
-
-demo2.addEventListener('click', function() {
-  demo2.classList.add('active');
-  demo1.classList.remove('active');
-  demo3.classList.remove('active');
-  mobile.classList.add('demo2');
-  mobile.classList.remove('demo1', 'demo3', 'navigation');
-}, false);
+link.addEventListener('click', function(e) {
+  e.preventDefault()
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
+})
