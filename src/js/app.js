251caa524f8e8f3592eee0acca8163ef53a15ac6
Olha Balumatkina <balumatkinaolga@gmail.com>
2023-01-09 17:20:17 +0200

    added mobile menu (not fixed)

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,20 @@ import '../styles/reset.scss';
 // window.addEventListener('load', () => {
 //     console.log(`Sum function 1+3 = ${sum(1,3)}`);
 // })
+
+// for mobile menu
+var burgerBtn = document.getElementById('burgerBtn');
+var mobile = document.getElementById('mobile');
+var demo2 = document.getElementById('demo2');
+
+burgerBtn.addEventListener('click', function() {
+  mobile.classList.toggle('navigation');
+}, false);
+
+demo2.addEventListener('click', function() {
+  demo2.classList.add('active');
+  demo1.classList.remove('active');
+  demo3.classList.remove('active');
+  mobile.classList.add('demo2');
+  mobile.classList.remove('demo1', 'demo3', 'navigation');
+}, false);
