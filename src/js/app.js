66a847981cbb2afe5233db8b1f9c66d2107c5c70
Olha Balumatkina <balumatkinaolga@gmail.com>
2023-01-10 12:00:59 +0200

    carousels transfered to js, added variables

--- src/js/app.js
+++ src/js/app.js
@@ -17,3 +17,69 @@ link.addEventListener('click', function(e) {
   burger.classList.toggle('open')
   ul.classList.toggle('open')
 })
+
+
+
+// carousel aircraft
+
+let aircraftWrap = document.getElementById("aircraft-flickity-wrap");
+// console.log('aircraftWrap: ' + typeof aircraftWrap);
+
+const pilotsWrap = document.getElementById("pilots-flickity-wrap");
+// console.log('pilotsWrap: ' + typeof pilotsWrap);
+
+const elAmount = 6;
+// console.log(typeof elAmount);
+
+const elTypeOneAircraft = '<div class="spin-wrap phone-reverse">\
+      <article class="spin-annotation aircraft-annotation">\
+        <a href="#" class="spin-name" aria-label="Aircraft Name"><h3>Aircraft Name</h3></a>\
+        <p class="spin-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed ac porttitor sapien. Nunc lorem tellus, interdum adui quis, vestibulum lobortis felis. <span class="hide-phone">Nullam porta pellentesque fermentum. Duis vehicula eros dolor, velaliquet tortor maximus eget. Praesent tempus diam ligula, ut varius nibh rhoncus non.</span></p>\
+        <button class="spin-button">SEE MORE</button>\
+      </article>\
+      <img class="spin-image" src="./images/webp/aircraft-1.webp" alt="Aircraft image" width="556" height="583" loading="lazy">\
+    </div>\
+    ';
+
+const elTypeTwoAircraft = '<div class="spin-wrap phone-reverse">\
+<article class="spin-annotation aircraft-annotation">\
+  <a href="#" class="spin-name" aria-label="Aircraft Name"><h3>Aircraft Name</h3></a>\
+  <p class="spin-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac porttitor sapien. Nunc lorem tellus, interdum a dui quis, vestibulum lobortis felis. <span class="hide-phone">Nullam porta p ellentesque fermentum. Duis vehicula eros dolor, vel aliquet tortor maximus eget. Praesent tempus diam ligula, ut varius nibh rhoncus non.</span></p>\
+  <button class="spin-button">SEE MORE</button>\
+</article>\
+<img class="spin-image" src="./images/webp/aircraft-2.webp" alt="Aircraft image" width="556" height="583" loading="lazy">\
+</div>\
+';
+
+const elTypeOnePilot = '<div class="spin-wrap">\
+<img class="spin-image" src="./images/webp/pilot-1.webp" alt="Pilot image" width="556" height="583" loading="lazy">\
+<article class="spin-annotation pilot-annotation">\
+  <a href="#" class="spin-name" aria-label="Pilot Name"><h3>First Second</h3></a>\
+  <p class="spin-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed ac porttitor sapien. Nunc lorem tellus, interdum adui quis, vestibulum lobortis felis. <span class="hide-phone">Nullam porta pellentesque fermentum. Duis vehicula eros dolor, velaliquet tortor maximus eget. Praesent tempus diam ligula,ut varius nibh rhoncus non.</span></p>\
+  <button class="spin-button">BOOK TIME</button>\
+</article>\
+</div>\
+';
+
+const elTypeTwoPilot = '<div class="spin-wrap">\
+<img class="spin-image" src="./images/webp/pilot-2.webp" alt="Pilot image" width="556" height="583" loading="lazy">\
+<article class="spin-annotation pilot-annotation">\
+  <a href="#" class="spin-name" aria-label="Pilot Name"><h3>First Second</h3></a>\
+  <p class="spin-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed ac porttitor sapien. Nunc lorem tellus, interdum adui quis, vestibulum lobortis felis. <span\ class="hide-phone">Nullam porta pellentesque fermentum. Duis vehicula eros dolor, velaliquet tortor maximus eget. Praesent tempus diam ligula,ut varius nibh rhoncus non.</span></p>\
+  <button class="spin-button">BOOK TIME</button>\
+</article>\
+</div>\
+';
+
+for (let i = 0; i < elAmount; i++){
+  if (i % 2 == 0) {
+    aircraftWrap.innerHTML += elTypeOneAircraft;
+    pilotsWrap.innerHTML += elTypeOnePilot;
+  } else {
+    aircraftWrap.innerHTML += elTypeTwoAircraft;
+    pilotsWrap.innerHTML += elTypeTwoPilot;
+  }
+}
+
+console.log("MOURICE I CAN'T");
+console.log("I CAN'T MOVE IT MOVE IT ANYMORE");
